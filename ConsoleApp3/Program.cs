a58dfe297c8e4d940f29a4ce51cc0bce5f559cf0
ymuzunburun <yasinuzunburun0@gmail.com>
2021-06-24 19:04:56 +0300

    first commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba, Hoşgeldiniz. Girdiğiniz iki sayıyının ortalamasını gösteren program.");
+            Console.WriteLine("1.sayıyı giriniz.");
+            double number1, number2, average;
+            number1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("2.sayıyı giriniz.");
+            number2 = Convert.ToDouble(Console.ReadLine());
+            
+            average = (number1 + number2) / 2;
+            Console.WriteLine("İki Sayının Ortalaması " + average);
+            
+        }
+    }
+}
